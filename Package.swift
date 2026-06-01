6d0dacc0f34808d971cbd995561cb2e5c727c149
sarias <sarias980@gmail.com>
2026-06-01 14:51:52 +0200

    Release 0.3.0

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ComposeApp",
-            url: "https://github.com/MagicFeedback/DeepdotsSDK-SPM/releases/download/0.2.2/DeepdotsSDK-0.2.2.xcframework.zip",
-            checksum: "cebee8ae4921d00b8dbec8fe21eba577cdbe3a481bfaf54c678d8327f1776db5"
+            url: "https://github.com/MagicFeedback/DeepdotsSDK-SPM/releases/download/0.3.0/DeepdotsSDK-0.3.0.xcframework.zip",
+            checksum: "9cccabcfd96ffb06e95e51a0690dd95e0707c90cc6fe72f6cfa350be80e5385f"
         )
     ]
 )
